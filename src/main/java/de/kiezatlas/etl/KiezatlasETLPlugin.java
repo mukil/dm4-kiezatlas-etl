1334dc10b6fde1468265f8102a00469df7171eb8
Malte Reißig <malte@mikromedia.de>
2016-05-25 17:33:48 +0200

    Added (untested) bezirk to site topic miration (Nr.11).

--- src/main/java/de/kiezatlas/etl/KiezatlasETLPlugin.java
+++ src/main/java/de/kiezatlas/etl/KiezatlasETLPlugin.java
@@ -4,16 +4,17 @@ import de.deepamehta.core.osgi.PluginActivator;
 import de.deepamehta.core.service.Inject;
 import de.deepamehta.plugins.accesscontrol.AccessControlService;
 import de.deepamehta.plugins.workspaces.WorkspacesService;
+import de.kiezatlas.KiezatlasService;
 
 
 public class KiezatlasETLPlugin extends PluginActivator {
 
     // --- Service Injection (and this class are just) here to provide these Services to Migration6 --- //
 
-    @Inject
-    private WorkspacesService workspaceService;
+    @Inject private WorkspacesService workspaceService;
 
-    @Inject
-    private AccessControlService accessControlService;
+    @Inject private AccessControlService accessControlService;
+
+    @Inject private KiezatlasService kiezService;
 
 }
