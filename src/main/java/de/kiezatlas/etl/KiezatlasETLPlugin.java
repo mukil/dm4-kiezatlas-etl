51c833d168d3f1a0090863c7502921dd99b6ad39
Malte Reißig <malte@mikromedia.de>
2017-05-26 16:06:20 +0200

    Bump version nr & Introduce sevice with etl facet-accessors.

--- src/main/java/de/kiezatlas/etl/KiezatlasETLPlugin.java
+++ src/main/java/de/kiezatlas/etl/KiezatlasETLPlugin.java
@@ -3,11 +3,16 @@ package de.kiezatlas.etl;
 import de.deepamehta.core.osgi.PluginActivator;
 import de.deepamehta.core.service.Inject;
 import de.deepamehta.accesscontrol.AccessControlService;
+import de.deepamehta.core.RelatedTopic;
+import de.deepamehta.core.Topic;
+import de.deepamehta.facets.FacetsService;
 import de.deepamehta.workspaces.WorkspacesService;
 import de.kiezatlas.KiezatlasService;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class KiezatlasETLPlugin extends PluginActivator {
+public class KiezatlasETLPlugin extends PluginActivator implements KiezatlasETLService {
 
     // --- Service Injection (and this class are just) here to provide these Services to Migration6 --- //
 
@@ -16,5 +21,29 @@ public class KiezatlasETLPlugin extends PluginActivator {
     @Inject private AccessControlService accessControlService;
 
     @Inject private KiezatlasService kiezService;
+
+    @Inject private FacetsService facets;
+
+    @Override
+    public Topic getFacetBeschreibung(Topic geoObject) {
+        return facets.getFacet(geoObject, BESCHREIBUNG_FACET);
+    }
+
+    @Override
+    public Topic getFacetStichworte(Topic geoObject) {
+        return facets.getFacet(geoObject, STICHWORTE_FACET);
+    }
+
+    @Override
+    public List<RelatedTopic> getAllCategories(Topic geoObject) {
+        List<RelatedTopic> cats = new ArrayList<RelatedTopic>();
+        List<RelatedTopic> themen = facets.getFacets(geoObject, THEMA_FACET);
+        List<RelatedTopic> zielgruppen = facets.getFacets(geoObject, ZIELGRUPPE_FACET);
+        List<RelatedTopic> angebot = facets.getFacets(geoObject, ANGEBOT_FACET);
+        cats.addAll(themen);
+        cats.addAll(zielgruppen);
+        cats.addAll(angebot);
+        return cats;
+    }
 
 }
